d76948aa4eb5975aedb7c3525c99b504265bd0c1
gopal <gopal1409@gmail.com>
2026-07-02 17:31:40 +0530

    added github action till plan for terraform

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -16,7 +16,7 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   #resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
-  subscription_id = "73cbb2ca-6092-4ae4-9e58-362b625a3b20"on ID
+  subscription_id = "73cbb2ca-6092-4ae4-9e58-362b625a3b20"
   features {}
 
 
